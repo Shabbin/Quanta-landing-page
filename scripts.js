02364b0c1e97e0b5a370f9f47610b09bf3a64300
Shabbin <ambiance0100@gmail.com>
2024-03-04 03:26:42 +0600

    1st-commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+const googleFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSfE0YXCL2AeJKYlTG2n4q7Oc_MF87JFTQwcenkFn8qZ6TciEg/viewform?usp=sf_link';
+
+document.getElementById('click-enroll').addEventListener('click', function () {
+
+
+    window.location.href = googleFormUrl;
+
+})
